782041418f9defc79edc8b3852ce17eedac62785
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-15 17:11:08 +0200

    restructured

--- src/App.js
+++ src/App.js
@@ -1,81 +1,26 @@
 import { useEffect, useRef } from "react";
-import { Scene } from "three";
-import createR from "./threeJsLib/renderer";
-import myCam from "./threeJsLib/camera";
-import createLights from "./threeJsLib/lights";
-import cubeTexture from "./threeJsLib/cubeTexture";
-import createPlane, {
-  bigBox,
-  moveBoxes,
-  someBoxes,
-} from "./threeJsLib/createPlaneAndBoxes";
+
 import SpinnerDots from "./Spinner/SpinnerDots";
-import setOrbitControls from "./threeJsLib/setOrbitControls";
+
+import setScene from "./threeJsLib/setScene";
 
 function App() {
   const rendererRef = useRef();
   const textRef = useRef();
   useEffect(() => {
-    //renderer
-    const renderer = createR();
-    //camera
-    const camera = myCam();
-    //scene
-    const scene = new Scene();
-    //lights
-    const lights = createLights();
-    scene.add(lights.directional);
-    scene.add(lights.ambient);
-
-    //add a plane
-    scene.add(createPlane());
-
-    //add the big box
-    scene.add(bigBox());
-
-    //add boxes
-    const boxes = [];
-    for (let x = -8; x < 8; x += 2) {
-      for (let y = -8; y < 8; y += 2) {
-        const box = someBoxes(x, y);
-        scene.add(box);
-        boxes.push([box, Math.random() * 2 > 1 ? 0.01 : -0.01, 0.5, 0]);
-      }
-    }
-
-    const onResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    };
+    const { domElement, onResize, animate } = setScene(textRef, rendererRef);
 
     //animate
     let frameId;
     const RAF = () => {
-      moveBoxes(boxes);
-      renderer.render(scene, camera);
-      controls.update();
+      animate();
       frameId = requestAnimationFrame(RAF);
     };
 
     //resize
     window.addEventListener("resize", onResize);
 
-    const { domElement } = renderer;
-
-    //add controls
-    const controls = setOrbitControls(camera, domElement);
-
-    //add renderer to DOM
-    const appender = () => {
-      textRef.current.remove();
-      rendererRef.current.appendChild(domElement);
-    };
-
-    //background, texture onLoad calls appender
-    scene.background = cubeTexture(appender);
-
-    //animate
+    //start animation
     RAF();
 
     //cleanup
@@ -92,7 +37,7 @@ function App() {
         style={{
           position: "fixed",
           bottom: "0",
-          left: "0",
+          left: "5px",
           color: "whitesmoke",
           fontSize: "10px",
         }}
---
+++ src/threeJsLib/setScene.js
@@ -0,0 +1,73 @@
+import { Scene } from "three";
+import myCam from "./camera";
+import createPlane, {
+  bigBox,
+  moveBoxes,
+  someBoxes,
+} from "./createPlaneAndBoxes";
+import cubeTexture from "./cubeTexture";
+import createLights from "./lights";
+import createR from "./renderer";
+import setOrbitControls from "./setOrbitControls";
+
+const setScene = (textRef, rendererRef) => {
+  //renderer
+  const renderer = createR();
+  //camera
+  const camera = myCam();
+  //scene
+  const scene = new Scene();
+  //lights
+  const lights = createLights();
+  scene.add(lights.directional);
+  scene.add(lights.ambient);
+
+  //add a plane
+  scene.add(createPlane());
+
+  //add the big box
+  scene.add(bigBox());
+
+  //add boxes
+  const boxes = [];
+  for (let x = -8; x < 8; x += 2) {
+    for (let y = -8; y < 8; y += 2) {
+      const box = someBoxes(x, y);
+      scene.add(box);
+      boxes.push([box, Math.random() * 2 > 1 ? 0.01 : -0.01, 0.5, 0]);
+    }
+  }
+
+  //add renderer to DOM
+  const appender = () => {
+    textRef.current.remove();
+    rendererRef.current.appendChild(domElement);
+  };
+
+  //background, texture onLoad calls appender
+  scene.background = cubeTexture(appender);
+
+  const { domElement } = renderer;
+
+  //add controls
+  const controls = setOrbitControls(camera, domElement);
+
+  //onResize
+  const onResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
+  //animate
+
+  const animate = () => {
+    moveBoxes(boxes);
+    renderer.render(scene, camera);
+    controls.update();
+  };
+
+  return { boxes, scene, camera, domElement, controls, onResize, animate };
+};
+
+export default setScene;
